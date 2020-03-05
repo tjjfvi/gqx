65aef855fd272ee499f46a6479973e74cfec7246
tjjfvi <tjjfvi@tjjfvi.com>
2020-03-05 15:58:39 -0500

    lint

--- src/stringifyObjectTypes/stringifyIds.ts
+++ src/stringifyObjectTypes/stringifyIds.ts
@@ -6,5 +6,5 @@ const str = (x: string) => JSON.stringify(x);
 
 export default (ids: Id[]) =>
   ids.map(id =>
-    `class ${stringifyId(id)} { private static _: any; static type = ${str(id.type)}; static prop = ${str(id.prop)}; }\n`
-  ).join("")
+    `class ${stringifyId(id)} { private static _: any; static type = ${str(id.type)}; static prop = ${str(id.prop)}; }`
+  ).join("\n")
