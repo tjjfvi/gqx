c23680fa540b865a504bf0a583c1b9e3f61b3f5b
tjjfvi <tjjfvi@tjjfvi.com>
2020-03-05 18:36:37 -0500

    exported a

--- src/stringifyObjectTypes/stringifyFragEnums.ts
+++ src/stringifyObjectTypes/stringifyFragEnums.ts
@@ -3,11 +3,13 @@
 import { Obj, Context } from ".";
 import stringifyId from "./stringifyId";
 
-export default (objs: Obj[], ctx: Context) =>
-  objs.map(obj => {
+export default (objs: Obj[], ctx: Context) => {
+  ctx.exports.push("$_");
+  return objs.map(obj => {
     ctx.exports.push(obj.type + "$");
     return `type ${obj.type}$ =\n` +
     obj.shallowProps.map(p => `  | typeof ${stringifyId(p.id)}\n`).join("") +
     obj.deepProps.map(p => `  | $<typeof ${stringifyId(p.id)}, ${p.type}$>\n`).join("")
   }).join("\n") +
     `\ntype $_ =${objs.map(o => `\n  | ${o.type}$`).join("")}`
+}
