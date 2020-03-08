045bc4ee71e6516ea7c017f0cf45f41f1a217a28
tjjfvi <tjjfvi@tjjfvi.com>
2020-03-08 17:11:37 -0400

    fixed bug in stringifyTypeVars

--- src/stringifyOperations/stringifyTypeVars.ts
+++ src/stringifyOperations/stringifyTypeVars.ts
@@ -6,6 +6,6 @@ import stringifyNamespace from "./stringifyNamespace";
 export default (objs: Obj[], ctx: Context) =>
   Object.entries(ctx.config.types || {}).map(([k, [f, e]]) =>
     stringifyNamespace(k, objs, (o, p) =>
-      `type ${p.id.prop} = ${f}<${stringifyId(p.id)}>;`
+      `type ${p.id.prop} = ${f}<typeof ${stringifyId(p.id)}>;`
     ).slice(e ? 0 : "export ".length)
   ).join("\n\n")
