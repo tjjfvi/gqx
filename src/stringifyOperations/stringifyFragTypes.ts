d620c7d7e903e573d2b42f77637069c8099cbfe0
tjjfvi <tjjfvi@tjjfvi.com>
2020-05-19 17:40:12 -0700

    $$Frag.$: never -> $$DeepArray<never>

--- src/stringifyOperations/stringifyFragTypes.ts
+++ src/stringifyOperations/stringifyFragTypes.ts
@@ -4,5 +4,5 @@ import stringifyNamespace from "./stringifyNamespace";
 
 export default (objs: Obj[], ctx: Context) =>
   stringifyNamespace("$$Frag", objs, (o, p) =>
-    `type ${p.id.prop} = ${(p.type in ctx.objectTypes ? `$$DeepArray<${p.type}$>` : "never")};`
+    `type ${p.id.prop} = $$DeepArray<${(p.type in ctx.objectTypes ? `${p.type}$` : "never")}>;`
   )
