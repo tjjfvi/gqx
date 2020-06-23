6c9399878e0496e7231379934380ee515fad44fa
tjjfvi <tjjfvi@tjjfvi.com>
2020-06-23 15:50:06 -0700

    added 90475OperationIds

--- src/stringifyOperations/stringifyProps.ts
+++ src/stringifyOperations/stringifyProps.ts
@@ -6,7 +6,11 @@ import indent from "../indent";
 const str = (x: string) => JSON.stringify(x);
 
 export default (objs: Obj[]) =>
-  objs.map(obj => obj.props.map(prop => `export class ${stringifyId(prop.id)} {
+  [
+    objs.map(obj =>
+      obj.props.map(prop =>
+        `
+export class ${stringifyId(prop.id)} {
   private static _: any;
   static typeProp = ${str(obj.prop)};
   static type = ${str(obj.type)};
@@ -14,4 +18,9 @@ export default (objs: Obj[]) =>
   static inputTypes = {${prop.args.length ? "\n" + prop.args.map(n =>
     indent(indent`${n.name.value}: ${str(n.type.loc.source.body.slice(n.type.loc.start, n.type.loc.end))},\n`)
   ).join("") + "  " : ""}};
-}\n\n`).join("")).join("");
+}\n\n`.trimStart()).join("")
+    ).join(""),
+    `type $$OperationIds = never\n`,
+    objs.map(obj => obj.props.map(prop => indent`| ${stringifyId(prop.id)}`).join("\n")).join("\n"),
+    "\n\n",
+  ].join("")
