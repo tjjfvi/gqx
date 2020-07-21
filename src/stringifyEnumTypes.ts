10d0bd8bcc82b216c194f8deedf8a1ef556bb9bd
tjjfvi <tjjfvi@tjjfvi.com>
2020-07-20 18:04:58 -0700

    added 24607EnumDirectives

--- src/stringifyEnumTypes.ts
+++ src/stringifyEnumTypes.ts
@@ -2,10 +2,22 @@
 import { Context } from ".";
 import { stringifyObject } from "./stringifyObject";
 import { stringifyEnum } from "./stringifyEnum";
+import { stringifyValue } from "./stringifyValue";
 
 export default (ctx: Context) =>
   Object.entries(ctx.enumTypes).map(([name, values]) =>
     `export type ${name} = ${stringifyEnum(values.map(v => JSON.stringify(v.name.value)))}`
   ).join("\n\n") + "\n\n" +
+  `export type $$EnumDirectives = ${stringifyObject(
+    Object.entries(ctx.enumTypes).map(([name, values]) =>
+      [name, stringifyObject(values.filter(v => v.directives?.length).map(value =>
+        [value.name.value, stringifyObject(value.directives?.map(dir =>
+          [dir.name.value, stringifyObject(dir.arguments?.map(arg =>
+            [arg.name.value, stringifyValue(arg.value)]
+          ) ?? [])]
+        ) ?? [])]
+      ))]
+    )
+  )}\n\n` +
   `export interface $$EnumTypes ${stringifyObject(Object.entries(ctx.enumTypes).map(([name]) => [name, name]))}`
 
